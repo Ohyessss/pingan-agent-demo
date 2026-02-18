bd66659ce330e6ae0ce001df9dd1955f5c228b34
Lucas <lucas@MacBook-Pro-2.local>
2026-02-18 15:24:16 +0800

    fix: 切换为 HashRouter，修复 GitHub Pages 路由 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ChatPage from './pages/ChatPage';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
@@ -6,7 +6,7 @@ import CustomerProfile from './pages/CustomerProfile';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/chat" replace />} />
         <Route path="/chat" element={<ChatPage />} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/customer" element={<CustomerProfile />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
